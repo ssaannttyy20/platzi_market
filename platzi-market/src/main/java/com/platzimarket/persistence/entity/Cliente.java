4621c3a6feb61f05c81c4aee0f06a14de7cc6326
Santiago Rodriguez <santiago.rodriguezg@usantoto.edu.co>
2022-05-12 11:39:52 -0500

    commit con la Api funcionando

---
+++ platzi-market/src/main/java/com/platzimarket/persistence/entity/Cliente.java
@@ -0,0 +1,76 @@
+package com.platzimarket.persistence.entity;
+
+
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
+@Table(name = "clientes")
+public class Cliente {
+
+    @Id
+    private String id;
+
+    private String nombre;
+
+    private String apellidos;
+
+    private Long celuar;
+
+    private String direcccion;
+
+    @Column(name = "correo_electronico")
+    private String correoElectronico;
+
+    @OneToMany(mappedBy = "cliente")
+    private List<Compra> compras;
+
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellidos() {
+        return apellidos;
+    }
+
+    public void setApellidos(String apellidos) {
+        this.apellidos = apellidos;
+    }
+
+    public Long getCeluar() {
+        return celuar;
+    }
+
+    public void setCeluar(Long celuar) {
+        this.celuar = celuar;
+    }
+
+    public String getDirecccion() {
+        return direcccion;
+    }
+
+    public void setDirecccion(String direcccion) {
+        this.direcccion = direcccion;
+    }
+
+    public String getCorreoElectronico() {
+        return correoElectronico;
+    }
+
+    public void setCorreoElectronico(String correoElectronico) {
+        this.correoElectronico = correoElectronico;
+    }
+}
